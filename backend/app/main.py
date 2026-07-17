ae673e5eb61c5e133abf6addd81dc0f9bb617f83
신준혁 <164439042+ldrugsnw@users.noreply.github.com>
2026-07-17 02:11:55 +0000

    feat: connect React frontend with FastAPI backend

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], # 나중에 보안을 위해서 origin은 하나만 넣어야됨
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def root():
